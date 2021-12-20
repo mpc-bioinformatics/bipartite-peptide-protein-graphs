f8c6f24485ed7ad3b052cc6b07c41785280aeeea
karin_schork <schork.karin@gmail.com>
2021-12-20 17:11:48 +0100

    rename tables and add table S4

--- Figures_and_Tables_for_Paper/Table_S1.R
+++ Figures_and_Tables_for_Paper/Table_S1.R
@@ -1,94 +1,43 @@
-library(openxlsx)
-library(xtable)
 
-D3_fasta <- read.xlsx("data/D3_without_isoforms/D3_fasta/table_subgraph_characteristics_D3_without_isoforms_fasta_min7AA.xlsx")
-isomorphlist_D3_fasta <- get(load("data/D3_without_isoforms/D3_fasta/isomorph_classes/isomorph_classes_merged_Peptides_D3_without_isoforms_fasta_min7AA_fast.RData"))
-D3_quant <- read.xlsx("data/D3_without_isoforms/D3_quant/table_subgraph_characteristics_D3_quant.xlsx")
-isomorphlist_D3_quant <- get(load("data/D3_without_isoforms/D3_quant/isomorph_classes/isomorph_classes_all_merged_Peptides.RData"))
-
-D3_iso_fasta <- read.xlsx("data/D3/D3_fasta/table_subgraph_characteristics_D3_fasta_min7AA.xlsx")
-isomorphlist_D3_iso_fasta <- get(load("data/D3/D3_fasta/isomorph_classes/isomorph_classes_merged_Peptides_D3_fasta_min7AA_fast.RData"))
-D3_iso_quant <- read.xlsx("data/D3/D3_quant/table_subgraph_characteristics_D3_quant.xlsx")
-isomorphlist_D3_iso_quant <- get(load("data/D3/D3_quant/isomorph_classes/isomorph_classes_all_merged_Peptides.RData"))
+### Numbers for Table S1
 
 
-################################################################################
-### function to calculate table:
+D1 <- read.table("data/D1/D1_quant/preprocessed/FC_NA_mind2.txt", sep = "\t", header = TRUE)
 
-table1_calculate_numbers <- function(D, isomorph) {
+## quantified peptides
+nrow(D1) # 22939
 
-  ind_zero_pep <- which(D$Nr_pep_node == 0)
-  if (length(ind_zero_pep) > 0) D <- D[-ind_zero_pep, ]
-
-  ### largest graph (in terms of number of protein nodes)
-  ind_largest <- which.max(D$Nr_prot_node)
-
-  ### 2nd largest graph (in terms of number of protein nodes)
-  ind_largest2 <- which(D$Nr_prot_node == sort(D$Nr_prot_node, decreasing = TRUE)[2])
-
-  c(
-    sum(D$Nr_prot_acc),                 ### Nr of proteins
-    sum(D$Nr_prot_node),                ### Nr of protein nodes
-    sum(D$Nr_pep_seq),                  ### Nr of peptides
-    sum(D$Nr_pep_node),                 ### Nr of peptide nodes
-    sum(D$Nr_edge),                     ### Nr of edges
-
-    as.integer(nrow(D)),                ### Nr of graphs
-    as.integer(sum(D$Nr_prot_node == 1 & D$Nr_pep_node == 1)),                 ### Nr of graphs with only 1 protein node
-    as.integer(length(isomorph$isomorph_list) - 1*(length(ind_zero_pep) > 0)), ### Nr of isomorphism classes
-
-    D$Nr_prot_node[ind_largest],        ### largest system
-    D$Nr_pep_node[ind_largest],
-    D$Nr_edge[ind_largest],
-
-    D$Nr_prot_node[ind_largest2],       ### 2nd largest system
-    D$Nr_pep_node[ind_largest2],
-    D$Nr_edge[ind_largest2]
-
-  )
-
-}
+## mean number of valid ratios
+mean(colSums(!is.na(D1[, -c(1:2)]))) # 18092.8
 
 
-################################################################################
-### Table S1: Comparison of D3 and D3_iso
 
-RES <- mapply(table1_calculate_numbers, D = list(D3_fasta, D3_iso_fasta, D3_quant, D3_iso_quant),
-              isomorph = list(isomorphlist_D3_fasta, isomorphlist_D3_iso_fasta, isomorphlist_D3_quant, isomorphlist_D3_iso_quant))
-rownames(RES) <- c("proteins", "protein nodes", "peptides", "peptide nodes", "edges", "graphs",
-                   "Graphs with 1 protein node",
-                   "isomorphism classes",  "protein nodes", "peptide nodes", "edges",
-                   "protein nodes", "peptide nodes", "edges")
 
-RES2 <- RES
-colnames(RES2) <- c("D3_fasta", "D3_iso_fasta", "D3_quant", "D3_iso_quant")
-write.xlsx(RES2, "Paper/Paper 1/tables/TableS1_Paper.xlsx", keepNA = TRUE,
-           row.names = TRUE, overwrite = TRUE)
+D2 <- read.table("data/D2_without_isoforms/D2_quant/preprocessed/FC_NA_mind2.txt", sep = "\t", header = TRUE)
 
-xtable(RES, digits = 0)
-# % latex table generated in R 4.1.2 by xtable 1.8-4 package
-# % Fri Dec 10 14:51:28 2021
-# \begin{table}[ht]
-# \centering
-# \begin{tabular}{rrrrr}
-# \hline
-# & 1 & 2 & 3 & 4 \\
-# \hline
-# proteins & 81548 & 103541 & 17585 & 22216 \\
-# protein.nodes & 80856 & 102463 & 10969 & 11672 \\
-# peptides & 3050340 & 3154386 & 30369 & 29490 \\
-# peptide.nodes & 148555 & 184932 & 10540 & 10895 \\
-# edges & 431391 & 647037 & 23802 & 27038 \\
-# graphs & 20270 & 20048 & 5267 & 5162 \\
-# Graphs.with.1.protein.node & 10129 & 8948 & 3315 & 3106 \\
-# isomorphism.classes & 4197 & 5656 & 454 & 517 \\
-# protein.nodes.1 & 6472 & 8895 & 57 & 58 \\
-# peptide.nodes.1 & 14993 & 19123 & 65 & 67 \\
-# edges.1 & 56950 & 87852 & 350 & 359 \\
-# protein.nodes.2 & 306 & 434 & 35 & 37 \\
-# peptide.nodes.2 & 757 & 940 & 34 & 34 \\
-# edges.2 & 2884 & 4613 & 181 & 185 \\
-# \hline
-# \end{tabular}
-# \end{table}
+## quantified peptides
+nrow(D2)  # 8101
+
+## mean number of valid ratios
+mean(colSums(!is.na(D2[, -c(1:2)])))  # 5618.639
+
+
+
+
+D3 <- read.table("data/D3_without_isoforms/D3_quant/preprocessed/FC_NA_mind2.txt", sep = "\t", header = TRUE)
+
+## quantified peptides
+nrow(D3)  # 46544
+
+## mean number of valid ratios
+sum(!is.na(D3[, -c(1:2)]))  # 30369
+
+
+D3_iso <- read.table("data/D3/D3_quant/preprocessed/FC_NA_mind2.txt", sep = "\t", header = TRUE)
+
+## quantified peptides
+nrow(D3_iso)  # 46544
+
+## mean number of valid ratios
+sum(!is.na(D3_iso[, -c(1:2)]))  # 29490
 
